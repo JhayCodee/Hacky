16c65b4cce5dc836132e5e9ca9e0c1ebfd7d6f7a
Luis Vargas <kluis0178@gmail.com>
2022-06-06 23:43:18 -0600

    SignUp screen terminada

--- app/src/main/java/com/hawk/hacky/SignUpActivity.kt
+++ app/src/main/java/com/hawk/hacky/SignUpActivity.kt
@@ -1,11 +1,22 @@
 package com.hawk.hacky
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hawk.hacky.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iniciarSesion.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
